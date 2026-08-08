7b70715a117ef766c3d35e805be256f7b5a95b9d
jababagaforoui-boop <jaba.bagaforo.ui@phinmaed.com>
2026-08-08 14:55:26 +0800

    Delete add_branch.php

--- add_branch.php
+++
@@ -1,8 +0,0 @@
-<?php
-include __DIR__ . '/../includes/db.php';
-
-if(isset($_POST['branch_name'])){
-    $branch_name = $conn->real_escape_string($_POST['branch_name']);
-    $conn->query("INSERT INTO branches (branch_name) VALUES ('$branch_name')");
-}
-?>
